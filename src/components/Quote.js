12d77da5e395f1625ed4eb800900a4503d389aab
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-07-14 16:29:02 -0500

    First version of project

---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Quote extends Component {
+  render() {
+    return (
+      <div className="col col-sm-3 box">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default Quote;

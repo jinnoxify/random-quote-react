931225c00a90af7d804bde3ab853cb37a0acf36e
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-07-16 21:58:20 -0500

    Updates

---
+++ precache-manifest.ac15369da69957a220c829e1d2f7c82b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0b7ec505ce50e29fddd1ab43b9b393",
+    "url": "/random-quote-react/index.html"
+  },
+  {
+    "revision": "072f2a4e926cef9d0313",
+    "url": "/random-quote-react/static/css/main.3de1e5d9.chunk.css"
+  },
+  {
+    "revision": "c43ee4f5172449fb92a6",
+    "url": "/random-quote-react/static/js/2.8fc8a7e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-react/static/js/2.8fc8a7e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072f2a4e926cef9d0313",
+    "url": "/random-quote-react/static/js/main.5eca76ed.chunk.js"
+  },
+  {
+    "revision": "a15c58c2deebc2eb8dbd",
+    "url": "/random-quote-react/static/js/runtime-main.c1074af7.js"
+  }
+]);
